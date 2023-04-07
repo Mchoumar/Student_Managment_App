a3ce8ca6ea9b39a9e24921bba7a58363dc866d27
choumar2001 <65606795+choumar2001@users.noreply.github.com>
2023-04-07 21:41:16 +0200

    added a new window, and new functionality

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QGridLayout, QPushButton, \
-    QLineEdit, QMainWindow, QTableWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QVBoxLayout, QPushButton, \
+    QLineEdit, QMainWindow, QTableWidget, QTableWidgetItem, QDialog, QComboBox
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 
 class MainWindow(QMainWindow):
@@ -17,6 +18,7 @@ class MainWindow(QMainWindow):
 
         # Adding extra menu items for file
         add_student_action = QAction("Add Student", self)
+        add_student_action.triggered.connect(self.insert)
         file_menu_item.addAction(add_student_action)
 
         # Adding extra menu item for help
@@ -27,16 +29,86 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
+        self.load_data()
 
         # Sets the widget as a central widget to the main window
-        self.setCentralWidget(self.table, 1)
-
+        self.setCentralWidget(self.table)
 
     def load_data(self):
-        self.table
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+
+        # Makes sure that it doesn't overwrite old data
+        self.table.setRowCount(0)
+
+        # Iterates through the data
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            print(row_number, row_data)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
+
+    def insert(self):
+        """Inserts new student's data into the table in the gui window"""
+        dialog = InsertDialog()
+        dialog.exec()
+
+
+class InsertDialog(QDialog):
+    """Creates a bew gui that takes students data"""
+    def __init__(self):
+        super().__init__()
+
+        # Set up for new window title and size
+        self.setWindowTitle("Insert Student Data")
+        self.setFixedWidth(300)
+        self.setFixedHeight(300)
+
+        # Layout for the new window
+        layout = QVBoxLayout()
+
+        # Name input
+        self.student_name = QLineEdit()
+        self.student_name.setPlaceholderText("Name")
+        layout.addWidget(self.student_name)
+
+        # Course input list
+        self.student_course = QComboBox()
+        self.student_course.addItems(["Biology", "Math", "Astronomy", "Physics"])
+        layout.addWidget(self.student_course)
+
+        # Mobile input
+        self.student_mobile = QLineEdit()
+        self.student_mobile.setPlaceholderText("Mobile")
+        layout.addWidget(self.student_mobile)
+
+        # Submit button used to submit the data into the database
+        submit_button = QPushButton("Submit")
+        submit_button.clicked.connect(self.add_student)
+        layout.addWidget(submit_button)
+
+        # Adding the widget into the new window
+        self.setLayout(layout)
+
+    def add_student(self):
+        name = self.student_name.text()
+        course = self.student_course.itemText(self.student_course.currentIndex())
+        mobile = self.student_mobile.text()
+        connection = sqlite3.connect("database.db")
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO students (name, course, mobile) VALUES(?, ?, ?)",
+                       (name, course, mobile))
+        connection.commit()
+        cursor.close()
+        connection.close()
+
+        # Refreshes the data after adding a student
+        main_window.load_data()
 
 
 app = QApplication(sys.argv)
-add_calculator = MainWindow()
-add_calculator.show()
+main_window = MainWindow()
+main_window.show()
 sys.exit(app.exec())
